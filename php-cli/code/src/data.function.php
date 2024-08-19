c51df89cd8835893597eb85114aab3add6dc3c87
Kulikov Alexander <shouter84@yandex.ru>
2024-08-19 22:44:40 +0300

    fixed some bugs

--- php-cli/code/src/data.function.php
+++ php-cli/code/src/data.function.php
@@ -7,13 +7,13 @@ function validateData(string $date): bool
         return false;
     }
 
-    if (isset($dataBlocks[0]) && $dateBlocks[0] > 31) {
+    if (isset($dateBlocks[0]) && $dateBlocks[0] > 31) {
         return false;
     }
-    if (isset($dataBlocks[1]) && $dateBlocks[0] > 12) {
+    if (isset($dateBlocks[1]) && $dateBlocks[1] > 12) {
         return false;
     }
-    if (isset($dataBlocks[2]) && $dateBlocks[2] > date('Y')) {
+    if (isset($dateBlocks[2]) && $dateBlocks[2] > date('Y')) {
         return false;
     }
     return true;
